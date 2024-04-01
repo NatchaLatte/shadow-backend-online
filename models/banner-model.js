93e006f52a32b88e7d4756d96c095056a75bd9a1
nutsang <foxteary@gmail.com>
2024-04-01 17:44:14 +0700

    banner

--- models/banner-model.js
+++ models/banner-model.js
@@ -1,15 +1,148 @@
-module.exports.bannerInsert = (request, response) => {
+const mysql = require('mysql2/promise')
+const assert = require('assert')
+const jsonwebtoken = require('jsonwebtoken')
+const SECRET = process.env.SECRET
+const multer = require('multer')
+const uuid = require('uuid')
+const path = require('path')
+const fs = require('fs')
+const storageBanner = multer.diskStorage({
+    destination: (request, file, callback) => {
+        callback(null, './public/images/banner')
+    },
+    filename: (request, file, callback) => {
+        const fileExtension = file.originalname.split('.')[1]
+        const fileName = `${uuid.v4()}${Date.now()}${Math.round(Math.random() * 1E9)}.${fileExtension}`
+        callback(null, fileName)
+        request.on('aborted', () => {
+            const fullPath = path.join('./public/images/banner', fileName)
+            fs.unlinkSync(fullPath)
+        })
+    }
+})
 
+const upload = multer({
+    storage: storageBanner,
+    fileFilter: (request, file, callback) => {
+        if(file.mimetype === 'image/png'){
+            callback(null, true)
+        }else{
+            callback(new Error('ใช้ได้แค่ไฟล์ .png เท่านั้น'), false)
+        }
+    }
+})
+
+module.exports.bannerInsert = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        upload.single('file')(request, response, async (error) => {
+            if(error){
+                response.status(200).json({status: false, payload: error.message})
+            }else{
+                const requestUUID = uuid.v4()
+                const requestInformation = request.file.filename
+                await connection.query('INSERT INTO banner (uuid, information) VALUES (?, ?)',
+                [requestUUID, requestInformation])
+                response.status(200).json({status: true, payload: 'การเพิ่มแถบประกาศสำเร็จ'})
+            }
+        })
+    }catch(error){
+        try{
+            fs.unlinkSync(path.join('./public/images/banner', request.file.filename))
+        }catch(error){}finally{
+            if(error.code === 'ECONNREFUSED'){
+                response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+            }else{
+                response.status(200).json({status: false, payload: 'การเพิ่มแถบประกาศล้มเหลว'})
+            }
+        }
+    }
 }
 
-module.exports.bannerSelect = (request, response) => {
-
+module.exports.bannerSelect = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const [results] = await connection.query('SELECT uuid, information, create_at, update_at FROM banner')
+        assert(results.length > 0)
+        response.status(200).json({status: true, payload: results})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การแสดงแถบประกาศล้มเหลว'})
+        }
+    }
 }
 
-module.exports.bannerUpdate = (request, response) => {
-
+module.exports.bannerUpdate = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        upload.single('file')(request, response, async (error) => {
+            if(error){
+                response.status(200).json({status: false, payload: error.message})
+            }else{
+                const requestUUID = request.body.uuid
+                const requestInformation = request.file.filename
+                const [results] = await connection.query('SELECT information FROM banner WHERE uuid = ?', [requestUUID])
+                assert(results.length > 0)
+                const information = results[0].information
+                fs.unlinkSync(path.join('./public/images/banner', information))
+                await connection.query('UPDATE banner SET information = ?, update_at = ? WHERE uuid = ?',
+                [requestInformation, new Date(), requestUUID])
+                response.status(200).json({status: true, payload: 'การแก้ไขแถบประกาศสำเร็จ'})
+            }
+        })
+    }catch(error){
+        try{
+            fs.unlinkSync(path.join('./public/images/banner', request.file.filename))
+        }catch(error){}finally{
+            if(error.code === 'ECONNREFUSED'){
+                response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+            }else{
+                response.status(200).json({status: false, payload: 'การแก้ไขแถบประกาศล้มเหลว'})
+            }
+        }
+    }
 }
 
-module.exports.bannerDelete = (request, response) => {
-
+module.exports.bannerDelete = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const requestUUID = request.params.uuid
+        const [results] = await connection.query('SELECT information FROM banner WHERE uuid = ?', [requestUUID])
+        assert(results.length > 0)
+        console.log(results)
+        const information = results[0].information
+        console.log(information)
+        await connection.query('DELETE FROM banner WHERE uuid = ?', [requestUUID])
+        fs.unlinkSync(path.join('./public/images/banner', information))
+        response.status(200).json({status: true, payload: 'การลบแถบประกาศสำเร็จ'})
+    }catch(error){
+        console.log(error)
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การลบแถบประกาศล้มเหลว'})
+        }
+    }
 }
--- models/game-name-model.js
+++ models/game-name-model.js
@@ -10,6 +10,29 @@ module.exports.gameNameUpdate = (request, response) => {
 
 }
 
-module.exports.gameNameDelete = (request, response) => {
-
+module.exports.gameNameDelete = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const requestEmail = request.body.email
+        const requestGiftTrueMoney = request.body.giftTrueMoney
+        const tw = await twApi(requestGiftTrueMoney, process.env.PHONENUMBER_RECIVE_MONEY)
+        assert(tw.status.code === 'SUCCESS')
+        const baht = tw.data.my_ticket.amount_baht
+        await connection.query('UPDATE finance SET cash_amount = cash_amount + ?, aysel_amount = aysel_amount + ?, update_at = ? WHERE email = ?',
+        [baht, (process.env.AYSEL_CURRENCY/process.env.BAHT_CURRENCY)*baht, new Date(), requestEmail])
+        await connection.query('INSERT INTO history_payment (uuid, email, aysel_amount, cash_amount, payment_status, create_at, update_at) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [uuid.v4(), requestEmail, (process.env.AYSEL_CURRENCY/process.env.BAHT_CURRENCY)*baht, baht, true, new Date(), new Date()])
+        response.status(200).json({status: true, payload: 'การเติมไอเซลสำเร็จ'})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การเติมไอเซลล้มเหลว'})
+        }
+    }
 }
