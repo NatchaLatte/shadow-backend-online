95227bc4401f1f1d27ec1b19c9454925f86a5dc0
nutsang <foxteary@gmail.com>
2024-04-01 18:32:03 +0700

    history-product

--- models/history-product-model.js
+++ models/history-product-model.js
@@ -1,15 +1,90 @@
-module.exports.createHistoryProduct = (request, response) => {
+const mysql = require('mysql2/promise')
+const jsonwebtoken = require('jsonwebtoken')
+const SECRET = process.env.SECRET
 
+module.exports.createHistoryProduct = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const requesUUID = request.body.uuid
+        const requesEmail = request.body.email
+        const requestGameName = request.body.game_name
+        const requestProductName = request.body.product_name
+        const requestProductPrice = request.body.product_price
+        const requestBuyMethod = request.body.buy_method
+        await connection.query('INSERT INTO history_product (uuid, email, game_name, product_name, product_price, buy_method, create_at) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [requesUUID, requesEmail, requestGameName, requestProductName, requestProductPrice, requestBuyMethod, new Date()])
+        response.status(200).json({status: true, payload: 'การสร้างประวัติธุรกรรมสำเร็จ'})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การสร้างประวัติธุรกรรมล้มเหลว'})
+        }
+    }
 }
 
-module.exports.readHistoryProduct = (request, response) => {
-
+module.exports.readHistoryProduct = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const token = request.cookies.token
+        const decoded = jsonwebtoken.verify(token, SECRET)
+        const requestEmail = decoded.email
+        const [results] = await connection.query('SELECT * FROM history_product WHERE email = ?',
+        [requestEmail])
+        response.status(200).json({status: true, payload: results})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การแสดงข้อมูลล้มเหลว'})
+        }
+    }
 }
 
-module.exports.readSumAysel = (request, response) => {
-
+module.exports.readSumAysel = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const [results] = await connection.query('SELECT SUM(product_price) AS sumAysel FROM history_product')
+        response.status(200).json({status: true, payload: results})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การแสดงข้อมูลล้มเหลว'})
+        }
+    }
 }
 
-module.exports.readSumBuyItems = (request, response) => {
-
+module.exports.readSumBuyItems = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const [results] = await connection.query('SELECT COUNT(uuid) AS sumBuyItem FROM history_product')
+        response.status(200).json({status: true, payload: results})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การแสดงข้อมูลล้มเหลว'})
+        }
+    }
 }
