45a2a62fbd5eb9850092e3723902a9ab06b078ec
nutsang <foxteary@gmail.com>
2024-04-01 18:26:12 +0700

    history-payment

--- models/history-payment-model.js
+++ models/history-payment-model.js
@@ -1,7 +1,45 @@
-module.exports.readHistoryPayment = (request, response) => {
+const mysql = require('mysql2/promise')
+const jsonwebtoken = require('jsonwebtoken')
+const SECRET = process.env.SECRET
 
+module.exports.readHistoryPayment = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const token = request.cookies.token
+        const decoded = jsonwebtoken.verify(token, SECRET)
+        const requestEmail = decoded.email
+        const [results] = await connection.query('SELECT aysel_amount, cash_amount, payment_status, create_at FROM history_payment WHERE email = ?',
+        [requestEmail])
+        response.status(200).json({status: true, payload: results})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การแสดงข้อมูลล้มเหลว'})
+        }
+    }
 }
 
-module.exports.readSumCash = (request, response) => {
-
+module.exports.readSumCash = async (request, response) => {
+    try{
+        const connection = await mysql.createConnection({
+            host        : process.env.DATABASE_HOST,
+            user        : process.env.DATABASE_USER,
+            password    : process.env.DATABASE_PASSWORD,
+            database    : process.env.DATABASE_NAME
+        })
+        const [results] = await connection.query('SELECT SUM(cash_amount) AS sumCash FROM history_payment')
+        response.status(200).json({status: true, payload: results})
+    }catch(error){
+        if(error.code === 'ECONNREFUSED'){
+            response.status(200).json({status: false, payload: 'เกิดข้อผิดพลาดขึ้นในการเชื่อมต่อกับฐานข้อมูล'})
+        }else{
+            response.status(200).json({status: false, payload: 'การแสดงข้อมูลล้มเหลว'})
+        }
+    }
 }
